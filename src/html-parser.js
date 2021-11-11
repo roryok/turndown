b7e344aee031d6c25211f4a986691ac8370f59d6
Rory <roryok@gmail.com>
2021-11-11 07:04:38 +0000

    force-domino

--- src/html-parser.js
+++ src/html-parser.js
@@ -8,39 +8,16 @@ var root = (typeof window !== 'undefined' ? window : {})
  * Parsing HTML strings
  */
 
-function canParseHTMLNatively () {
-  var Parser = root.DOMParser
-  var canParse = false
-
-  // Adapted from https://gist.github.com/1129031
-  // Firefox/Opera/IE throw errors on unsupported types
-  try {
-    // WebKit returns null on unsupported types
-    if (new Parser().parseFromString('', 'text/html')) {
-      canParse = true
-    }
-  } catch (e) {}
-
-  return canParse
-}
-
 function createHTMLParser () {
   var Parser = function () {}
-  if (process.browser) {
-    Parser.prototype.parseFromString = function (string) {
-      var doc = document.implementation.createHTMLDocument('')
-      doc.open()
-      doc.write(string)
-      doc.close()
-      return doc
-    }
-  } else {
-    var domino = require('domino')
-    Parser.prototype.parseFromString = function (string) {
-      return domino.createDocument(string)
-    }
+  Parser.prototype.parseFromString = function (string) {
+    var doc = document.implementation.createHTMLDocument('')
+    doc.open()
+    doc.write(string)
+    doc.close()
+    return doc
   }
   return Parser
 }
 
-export default canParseHTMLNatively() ? root.DOMParser : createHTMLParser()
+export default createHTMLParser()
